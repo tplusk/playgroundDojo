bc65244894e8cdf88ac60c201742990d32b4a229
tplusk <macfelelhun@gmail.com>
2025-03-06 16:34:00 +0000

    trying to understand how functions work

--- dojo2.js
+++ dojo2.js
@@ -1,8 +1,11 @@
 const fruits = ['apple', 'lemon', 'banana', 'strawberry', 'BaNAnA', 'kiwi']
 
+const oneApple = (element) => element === 'apple';
+
 function findMore(fruits) {    
     const matches = /banana/gi.test(fruits);
     // console.log(fruits.indexOf('banana'));
+
     return matches;
 }
 console.log(findMore(fruits));
@@ -10,3 +13,6 @@ console.log(findMore(fruits));
 // console.log(fruits[0]);
 console.log(fruits[2]);
 console.log(fruits[5]);
+console.log(' ');
+console.log(fruits.some(oneApple));
+console.log(fruits.indexOf('apple'));
